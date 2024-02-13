7c819111c9f3421f8c54bd43681e476f24d644c2
Freeborger <ben.and.david.walker@gmail.com>
2024-02-13 12:07:44 -0700

    adlib

--- controllers/index.js
+++ controllers/index.js
@@ -35,7 +35,35 @@ const game = (req, res) => {
                     <body>
                         <p id="adlib">plants</p>
                     </body>
-                    <script></script>
+                    <script>
+                    
+//prompts the questions
+
+let noun = prompt("give me a noun")
+
+let nounPlural = prompt("give me plural noun")
+
+let verb = prompt("give me a verb (don't end it in ing or s)")
+
+let adjective = prompt("give me an adjective")
+
+let person = prompt("give me a person")
+
+
+//picks a random adlib
+
+
+let num = Math.floor(Math.random() * 2);
+
+//stores the adlibs
+let adlibs = [${verb}ing is an ${adjective} way to attract ${nounPlural}!, ${person} uses ${noun} to attract ${nounPlural} all the time!,
+${person} often ${verb}s as a part of his ${noun}, he does it to earn ${nounPlural} so he can buy ${adjective} clown noses,
+${person} advises you often ${verb}, to be at your best possible ${adjective}ness.${noun} often play a key role in this and this plays a major role in getting ${nounPlural}]
+
+
+
+//generates your adlib
+document.getElementById("adlib").innerHTML = adlibs[num]</script>
 
                 </html>`)
 
