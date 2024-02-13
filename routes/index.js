bca01a49b6b5269c0c938c7bc0e371eabaa3341b
Freeborger <ben.and.david.walker@gmail.com>
2024-02-13 12:09:07 -0700

    adlib game

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const routes = require("express").Router()
 
 routes.get("/", myController.awesomeFunction)
 routes.get("/tooele", myController.makeBtn)
+routes.get("/adlib", myController.game)
 
 
 
