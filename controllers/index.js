74280966f6a2db5f448d8786c294ef59489eddb3
Freeborger <ben.and.david.walker@gmail.com>
2024-02-13 12:04:32 -0700

    adlib

--- controllers/index.js
+++ controllers/index.js
@@ -10,7 +10,7 @@ const awesomeFunction = (req, res) => {
 
 const makeBtn = (req, res) => {
 
-    res.send('<button id="btn">PRESS ME</button>')
+    res.send('Tooele tech rules')
 
     document.querySelector('#btn').addEventListener('click', (e) => {
 
@@ -18,6 +18,27 @@ const makeBtn = (req, res) => {
     })
 
     console.log('sucess')
+}
+
+const game = (req, res) => {
+
+    res.send(`<!DOCTYPE html>
+        < html lang = "en" >
+
+        <head>
+            <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Document</title>
+                    <link rel="stylesheet" href="style.css">
+                    </head>
+
+                    <body>
+                        <p id="adlib">plants</p>
+                    </body>
+                    <script></script>
+
+                </html>`)
+
 }
 
 module.exports = { awesomeFunction, makeBtn }
